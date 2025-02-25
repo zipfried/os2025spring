9a81081b7d67e4b8e3b35618aa6ddb19747afcd3
zipfried <richardzipfried@gmail.com>
2025-02-26 00:49:45 +0800

    use default test data

--- src/lru/main.c
+++ src/lru/main.c
@@ -9,12 +9,8 @@ int main() {
   const int FRAME_COUNT = 3;
   stack_init(FRAME_COUNT);
 
-  const int TEST_DATA[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1,
-                           7, 0, 1, 7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1,
-                           2, 0, 1, 7, 0, 1, 7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0,
-                           3, 2, 1, 2, 0, 1, 7, 0, 1, 7, 0, 1, 2, 0, 3, 0, 4,
-                           2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1, 7, 0, 1, 2, 0,
-                           3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
+  const int TEST_DATA[] = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3,
+                           0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
   const size_t TOTAL_COUNT = sizeof(TEST_DATA) / sizeof(int);
 
   size_t err_count = 0;
